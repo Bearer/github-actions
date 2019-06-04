7e146a039d20c8a907e1d6d348007f5d78073725
Tanguy Antoine <antoine@bearer.sh>
2019-06-04 14:30:50 +0200

    feat: allow to specify a version

--- invoke/entrypoint.sh
+++ invoke/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 BEARER_HOST=${HOST-"http://int.bearer.sh"}
+API_VERSION=${VERSION-"v5"}
 PAYLOAD=${DATA-"{}"}
 
 : ${BEARER_API_KEY:?"Need to set BEARER_API_KEY non-empty"}
@@ -13,19 +14,16 @@ response=$(curl -X POST -s \
   -d "$PAYLOAD" \
   -H "Content-Type: application/json" \
   -H "Authorization: $BEARER_API_KEY" \
-  "$BEARER_HOST/api/v5/functions/backend/${UUID}/$FUNCTION_NAME?authId=$AUTH_ID")
+  "$BEARER_HOST/api/${API_VERSION}/functions/backend/${UUID}/${FUNCTION_NAME}?authId=${AUTH_ID}")
 
 error=$(echo $response | jq .error)
 data=$(echo $response | jq .data)
 
-if [[ "$error" != "null" ]]
-then
+if [[ "$error" != "null" ]]; then
   echo $error
   exit 1
 fi
 
-if [[ "${LOG_LEVEL:-none}" == "DEBUG" ]]
-then
+if [[ "${LOG_LEVEL:-none}" == "DEBUG" ]]; then
   echo $data
 fi
-
